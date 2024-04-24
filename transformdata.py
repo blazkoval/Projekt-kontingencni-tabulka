dcf5c5025cc79beffcd075e7e46772a9a0285e05
blazkoval <blazkovalenka2@gmail.com>
2024-04-24 23:36:01 +0200

    duration added

--- transformdata.py
+++ transformdata.py
@@ -1,5 +1,6 @@
 import json
-# "D:\projekt-kont-tabulka\Projekt-kontingencni-tabulka\Projekt-kontingencni-tabulka\data.json"
+from datetime import datetime
+
 f = "data.json"
 with open(f, 'r', encoding='utf-8') as file:
     data_json = json.load(file)
@@ -10,6 +11,15 @@ for event in events:
     event_type = event.pop("eventType")  # Odstranění původního eventType
     event["eventtype_id"] = event_type["id"]  # Přidání nového klíče eventtype_id
     event["eventtype_name"] = event_type["name"]  # Přidání nového klíče eventtype_name
+
+    start_date = datetime.fromisoformat(event["startdate"])
+    end_date = datetime.fromisoformat(event["enddate"])
+    duration = end_date - start_date
+    event["duration"] = str(duration)
+
+    # Odstranění původních atributů "startdate" a "enddate"
+    del event["startdate"]
+    del event["enddate"]
 
 with open('transformed.json', 'w', encoding='utf-8') as outfile:
     json.dump(data_json, outfile, indent=4, ensure_ascii=False)
